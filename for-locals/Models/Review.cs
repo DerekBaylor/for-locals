b888b0c83cacf68c9b3dcf40f5e17b51d529b794
Derek Baylor <dbaylor25@gmail.com>
2022-05-22 17:56:11 -0500

    created models

---
+++ for-locals/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace for_locals.Models
+{
+    public class Review
+    {
+        public string ReviewId { get; set; }
+        public string UserId { get; set; }
+        public string BusinessId { get; set; }
+        public string ReviewText { get; set; }
+        public string ImgUrl { get; set; }
+
+    }
+}
