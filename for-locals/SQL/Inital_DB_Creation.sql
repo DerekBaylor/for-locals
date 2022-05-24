5652edd7ce2b09aecc0c45c16f0fa83772312e24
Derek Baylor <dbaylor25@gmail.com>
2022-05-24 12:27:14 -0500

    Added some mock data

--- for-locals/SQL/Inital_DB_Creation.sql
+++ for-locals/SQL/Inital_DB_Creation.sql
@@ -1,41 +1,49 @@
 ﻿USE ForLocals
 GO
 
-DROP TABLE IF EXISTS Bussiness;
 DROP TABLE IF EXISTS [User];
+DROP TABLE IF EXISTS Business;
 DROP TABLE IF EXISTS Review;
 
 CREATE TABLE [User] (
         UserId INTEGER NOT NULL PRIMARY KEY IDENTITY,
         [Name] VARCHAR(255),
         ImgUrl VARCHAR,
-        Bio VARCHAR(255),
+        Bio VARCHAR(255) DEFAULT 'Hello World' NOT NULL,
         IsAdmin CHAR(1) DEFAULT 'N' NOT NULL,
 );
 
 CREATE TABLE Business (
 	    BusinessId INTEGER NOT NULL PRIMARY KEY IDENTITY,
-        StateControlNum VARCHAR(55) NOT NULL,
+        StateControlNum VARCHAR(9) NOT NULL,
         BusinessName VARCHAR(255) NOT NULL,
         Phone VARCHAR(55),
         [Address] VARCHAR(255) NOT NULL,
-        [Description] VARCHAR NOT NULL,
+        [Description] VARCHAR(255) DEFAULT 'Hello World' NOT NULL,
         Keywords VARCHAR(255) NOT NULL,
         Industry VARCHAR(255) NOT NULL,
         Logo VARCHAR,
         ImgURL VARCHAR,
         ReviewScore INTEGER, 
         Verified CHAR(1) DEFAULT 'N' NOT NULL,
-        CONSTRAINT FK_User_Id FOREIGN KEY (UserId) REFERENCES [User](Id), 
+        UserId VARCHAR NOT NULL, 
 );
 
 CREATE TABLE Review (
-        ReviewId VARCHAR NOT NULL PRIMARY KEY IDENTITY,
+        ReviewId INTEGER NOT NULL PRIMARY KEY IDENTITY,
         ReviewText VARCHAR NOT NULL,
         ImgUrl VARCHAR,
         Score INTEGER NOT NULL,
-        CONSTRAINT FK_Reviewer_Id FOREIGN KEY (UserId) REFERENCES [User](UserId),
-        CONSTRAINT FK_Business_Id FOREIGN KEY (BusinessId) REFERENCES Business(BusinessId),
+        UserId VARCHAR NOT NULL,
+        BusinessId VARCHAR NOT NULL,
 );
 
-INSERT INTO [USER] ([Name], ImgUrl, Bio, IsAdmin) VALUES ('Derek B', 'https://wgfuljukrcejubfajlyl.supabase.co/storage/v1/object/sign/images/BlankProfileImage.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvQmxhbmtQcm9maWxlSW1hZ2UucG5nIiwiaWF0IjoxNjUzMzU2ODE4LCJleHAiOjE5Njg3MTY4MTh9.HVj6zriVzWXj5qZHceNEvTVvdZOgN7dHH1rUAnXg_lg','Hello World','Y');
+-- Mock User Data
+INSERT INTO [USER] ([Name], Bio, IsAdmin) VALUES ('Derek B', 'Im the admin. Welcome to the site!','Y');
+INSERT INTO [USER] ([Name], Bio) VALUES ('John Smith', 'I am the owner of Pick Tennessee!');
+
+-- Mock Business Data
+INSERT INTO Business (StateControlNum, BusinessName, [Address], Keywords, Industry, UserId) 
+VALUES ('000607957', 'Pick Tennessee Conference','6290 McDaniel Rd. College Grove, TN 37046', 'Agriculture, Farming', 'Agriculture', '2');
+
+-- Mock Review Data
