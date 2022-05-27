3be2c33e790c2dcfe79c2e32aa57f1afdc9ac691
Derek Baylor <dbaylor25@gmail.com>
2022-05-26 21:23:19 -0500

    Created Repo for Reviews

---
+++ for-locals/Repositories/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using for_locals.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace for_locals.Repositories
+{
+    public interface IReviewRepository
+    {
+        void AddReview(Review review);
+        void DeleteReview(int reviewId);
+        List<Review> GetUserReviewsByBusinessId(string businessId);
+    }
+}
