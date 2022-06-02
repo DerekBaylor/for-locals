9dc6f565bcebe4c9c1eeab474fa910b0fca1bb3f
Derek Baylor <dbaylor25@gmail.com>
2022-06-01 20:34:45 -0500

    featured business card

--- forlocalsclient/src/Components/FeatureBusinessCard.js
+++ forlocalsclient/src/Components/FeatureBusinessCard.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from 'react';
+import DefaultBusinessImg from '../Assets/DefaultBusinessImg.jpg'
 
 export default function FeatureBusinessCard() {
   return (
-    <div className="card featured-business-card" >
-        <img className="card-img-top" src="..." alt="Card image cap" />
-        <div className="card-body">
-            <h5 className="card-title">Featured Business</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+    <div className="card featured-business-card">
+    <div className="card-div-two">
+      <img className="featured-card-img" src={DefaultBusinessImg} alt="featured business" />
+      <div className="card-body">
+          <h5 className="card-title">Featured Business</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="card-btn-div">
+          <a href="#" className="btn btn-primary card-btn">Details</a>
         </div>
+      </div>
     </div>
+  </div>
   )
 }
