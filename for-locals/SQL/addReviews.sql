5e2a6b902bf5adf6728258bb2d0c9eacbb40de9f
Derek Baylor <dbaylor25@gmail.com>
2022-06-21 08:07:02 -0500

    fixed bus form error

---
+++ for-locals/SQL/addReviews.sql
@@ -0,0 +1,29 @@
+﻿USE ForLocals
+GO
+
+DROP TABLE Review;
+
+CREATE TABLE Review (
+        ReviewId INT NOT NULL PRIMARY KEY IDENTITY,
+        ReviewTitle VARCHAR(100) NOT NULL,
+        ReviewText VARCHAR(1000) NOT NULL,
+        ImgUrl VARCHAR(500) DEFAULT 'https://zfjepfckphrvbatmvyud.supabase.co/storage/v1/object/sign/for-locals-img/ReviewIcon.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJmb3ItbG9jYWxzLWltZy9SZXZpZXdJY29uLnBuZyIsImlhdCI6MTY1NTU1ODA5NCwiZXhwIjoxOTcwOTE4MDk0fQ.phspICuNYjbQisR_WkhL9PfFikqgDCBhMmXlAt4_HQc',
+        Score INTEGER NOT NULL,
+        UserId INT NOT NULL,
+        BusinessId INT NOT NULL,
+);
+
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('Great Business', 'This is a great business!', 5, 1, 1);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('Love the People', 'I love shopping here. Great people!', 5, 1, 6);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('Great Service', 'They treat me great', 5, 1, 20);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('I scream for icecream', 'Best Ice Cream in town!', 5, 3, 12);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('Great Food', 'Id eat here any day!', 5, 7, 4);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('They get busy.', 'Great food, long line!', 4, 6, 4);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('Great Business', 'This is a great business!', 4, 3, 19);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('Great Business', 'This is a great business!', 4, 2, 1);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('Meh', 'I kind of like it.', 3, 3, 1);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('Cold Food', 'My sandwhich was cold.', 4, 7, 17);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('Great Business', 'This is a great business!', 5, 2, 8);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('Great Business', 'This is a great business!', 5, 6, 8);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('Love them meat!', 'They have the meats!', 5, 10, 9);
+INSERT INTO Review (ReviewTitle, ReviewText, Score, UserId, BusinessId) VALUES ('My Favorite Shop!', 'I shop here every week.', 5, 11, 9);
--- forlocalsclient/src/Views/AddBusiness.js
+++ forlocalsclient/src/Views/AddBusiness.js
@@ -1,15 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import React from 'react'
+import PropTypes from 'prop-types';
 import BusinessForm from '../Components/BusinessForm'
 
-export default function AddBusiness() {
-    const { firebaseKey } = useParams();
+export default function AddBusiness(count, setcount) {
 
   return (
     <div className='main-body-div'>
     <div className='navbar-spacing'></div>
     Add Your Business
-        <BusinessForm />
+        <BusinessForm 
+          count={count}
+          setcount={setcount}
+        />
     </div>
   )
 }
+
+AddBusiness.propTypes = {
+  count: PropTypes.number.isRequired,
+  setcount: PropTypes.func.isRequired,
+};
